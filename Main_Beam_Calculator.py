152822ac05c95a3e65438654144c55a3053905d4
mitchellhshap <57406657+mitchellhshap@users.noreply.github.com>
2025-07-10 12:13:28 -0400

    Finalize auto-density functions and implement JSON

--- Main_Beam_Calculator.py
+++ Main_Beam_Calculator.py
@@ -1,13 +1,18 @@
 # Developed by Mitch S-A
-# Updated on July 8, 2025
-# Usage Notes:
+# Updated on July 10, 2025
+# Usage Notes: Don't
 # Dev Notes: Implement an interference checker when the x-ray beam source is selected (e.g. raise flag if Cu X-rays are used with Fe species)
 # Status: Non-functional, unmanaged git integration
 
+# To-Do:
+# Finalize density calculation section (line 123)
+
 # ---------- Necessary imports ----------
 
-import csv
 import chemparse
+import json
+
+# ---------- Class Definitions ----------
 
 # ---------- Simplifying functions ----------
 
@@ -27,6 +32,50 @@ def chem_form_parser(formula):
         print(f"Error parsing formula: {e}")
         return {}
 
+# Generates dictionary of relevant atomic information based on a stoich dictionary from chem_form_parser
+def get_atomic_info(stoich_dict):
+    atomic_info_dictionary = {}  # Establish desired dictionary as empty
+    with open("Element_Densities_and_Weights.json", 'r') as jsonfile:  # Read in .json with necessary information
+        element_data_dict = json.load(jsonfile)
+        for element in stoich_dict.keys():  # Pull each unique element from input stoich_dict (e.g "Ca")
+            # element_data_dict[element] yields the list of values for that element of the form /
+            # ['Z', 'Element', 'Z/A', 'I (eV)', 'Density (g/cm3)', 'Molecular Weight (g/mol)']
+            atomic_info_dictionary[element] = element_data_dict[element]  # Note this is a list of strings still!
+    # return an abridged atomic info dictionary containing only information for relevant elements
+    # avoids reading in the same large .json file multiple times
+    return atomic_info_dictionary
+
+# Takes a stoich dictionary from chem_form_parser and atomic info from .json file returns a total mass
+def molecular_weight_total(stoich_dict, atomic_info):
+    molecular_weight_sum = 0 # Establish the desired variable with zero value
+    for element in stoich_dict.keys(): # Recall the keys of both passed dictionaries match
+        # atomic_info[element] yields the list of str values for that element of the form /
+        # ['Z', 'Element', 'Z/A', 'I (eV)', 'Density (g/cm3)', 'Molecular Weight (g/mol)']
+        molecular_weight_sum += float(atomic_info[element][5]) * float(stoich_dict[element]) # molecular weight * stoich abundance
+    return molecular_weight_sum
+
+# Function to calculate the weighted average density from stoichiometry
+def density_calc(stoich_dict, atomic_info): # Takes a dictionary from chem_form_parser
+    # Establish returnable density variable
+    calculated_density = 0
+    # Determine the sample's total molecular weight
+    sample_molecular_weight = molecular_weight_total(stoich_dict, atomic_info)
+    # Write a master for loop to calculate the gram-based percent abundance of the element and multiple by density
+    for element in stoich_dict.keys(): # recall stoich_dict.keys() is equivalent to atomic_info.keys()
+        gram_percent_abundance = (float(stoich_dict[element]) * float(atomic_info[element][5])) / sample_molecular_weight
+        calculated_density += gram_percent_abundance * float(atomic_info[element][4])
+    return calculated_density
+
+# ---------- Function debugging section ----------
+
+# sample_CaCO3_dict = {'Ca': 1.0, 'C': 1.0, 'O': 3.0}
+# sample_metal_dict = {'Fe': 5.0, 'Cu': 3.0, 'O': 20.0, 'C':6}
+#
+# master_CaCO3_dict = get_atomic_info(sample_CaCO3_dict)
+# master_metal_dict = get_atomic_info(sample_metal_dict)
+# print(density_calc(sample_CaCO3_dict, master_CaCO3_dict))
+# print(density_calc(sample_metal_dict, master_metal_dict))
+
 # ---------- Begin user facing code ----------
 # Welcome message
 
@@ -35,48 +84,53 @@ optics settings for your sample, sample holder, and diffractometer when collecti
 
 # Get chemical formula from user. Code below will repeat until a satisfactory element dictionary is generated.
 
-element_dict_empty = True
-while element_dict_empty == True:
+element_dict = {}
+while not element_dict:
     chemical_formula = input("Please enter your sample's known or approximate chemical formula: ")
     element_dict = chem_form_parser(chemical_formula)
     if element_dict != {}:
         user_confirmed = False
-        while user_confirmed == False:
+        while not user_confirmed:
             print("Please confirm the following elemental breakdown: ")
             for key,value in element_dict.items():
                 print(str(key), str(value))
             user_confirmation = input("Is this correct? y/n: ")
             if user_confirmation == "y":
                 user_confirmed = True
-                element_dict_empty = False
             elif user_confirmation == "n":
-                break
+                element_dict = {}
+                user_confirmed = True
             else:
                 print("Invalid input. Please enter y or n.")
     else:
         continue
 
 print("Dictionary conversion successful.")
-#Debugging print:
-print(element_dict)
 
-# Now, prompt for density, and store a user given value.
-# If no user has no value, calculate a weighted average from Elemental_Densities.
+# Pull dictionary of relevant atomic information based on the user input
+
+print("Gathering information about the atoms in your sample...")
+try:
+    master_atomic_info = get_atomic_info(element_dict)
+except Exception as e:
+    print("An error occurred: {e}. Density calculation for your sample unavailable.".format(e=e))
+    master_atomic_info = None
+
+# Loop to determine density of sample
+
+true_density = 0 # Establish the density to be used in penetration depth calculations
+density_user_confirmation = False # Establish a bool to ensure the user can confirm their input
+while true_density == 0 and not density_user_confirmation:
+    if master_atomic_info is None: # Code to use if the user's sample does not generate atomic substituent dictionary
+        print("You will need to provide your sample's density or best estimate of it in g/cm^3.")
+        user_input_density = input("Please enter your sample's density (g/cm^3): ")
+        # Code to ask for user estimated density and confirm with user
+    elif master_atomic_info is not None:  # Code to use if the user's sample does generate atomic substituent dictionary
+        print("You may choose to provide your sample's density or use a linear combination of densities from atomic constituents.")
+        print("Note that best results are obtained with a literature-based or empirically determined sample density.")
+        calculated_density = density_calc(element_dict, master_atomic_info)
+        print("Your sample's density is {:.2f} g/cm^3.".format(calculated_density))
+        density_user_choice = input("Enter your own (c)ustom density or use the (p)rovided density? c/p ")
+
+
 # Tell user what the assumed density will be and proceed.
-
-
-
-
-# sample_density = 0.0
-# density_acquired = False
-# while density_acquired == False:
-#     density_prompt = input("Is your density known? y/n: ")
-#     if density_prompt == "y":
-#         sample_density = input("Enter your sample density (g/cm^3): ")
-#         density_acquired = True
-#     elif density_prompt == "n":
-#         # Change below to throw to density calculator
-#         sample_density = 0
-#         density_acquired = True
-#     else:
-#         print("Invalid input. Please enter y or n.")
